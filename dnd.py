5dbdf95e4084c8a92e2d18f724cd526d756d0af2
Wyine <kashahat@gmail.com>
2017-08-16 12:03:01 -0500

    Rolls any number sided die based off of user input

--- dnd.py
+++ dnd.py
@@ -2,11 +2,17 @@
 
 import random as rand
 
-def rolldice(upperPlusOne):
-    rand.seed()
-    return rand.randrange(1, upperPlusOne)
+def rolldice(diceRoll):
+    print("d%d roll: " % (diceRoll), end="")
+    return rand.randrange(1, diceRoll+1)
 
 
 
 if __name__ == '__main__':
-    print(rolldice(21))
+    rand.seed()
+    rollNum = 0
+    while True:
+        rollNum = int(input("What kind of dice would you like to roll? -1 to exit: "))
+        if rollNum == -1: break
+        print(rolldice(rollNum))
+    print("Thanks for playing. Hope you didn't die!")
